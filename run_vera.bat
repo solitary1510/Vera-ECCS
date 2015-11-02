5df285b74e101beadf7ad8052ae82fbb8466185d
Phuong H.D Nguyen <solitary1510@gmail.com>
2015-11-02 22:54:51 +0700

    Using group rules

--- run_vera.bat
+++ run_vera.bat
@@ -1,5 +1,5 @@
 @echo off
 set PATH=%PATH%;".\vera++";".\vera++\bin";
 set VERA_ROOT=.\1212299_1212504
-vera++ -p full_vera_exam code_need_check.c
+vera++ -p 1212299_1212504_rules code_need_check.c
 PAUSE
