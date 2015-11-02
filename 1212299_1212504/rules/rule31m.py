32164940a4b5de7745fd502df9c76231f766e65a
Phuong H.D Nguyen <solitary1510@gmail.com>
2015-11-02 23:25:24 +0700

    Added rule 3.1.m

---
+++ 1212299_1212504/rules/rule31m.py
@@ -0,0 +1,17 @@
+# Rule 3.1.m: Each semicolon shall follow the statement it terminates without a precceding space
+
+for filename in vera.getSourceFileNames():
+    file = open(filename, 'rb');
+    lineCounter = 1;
+    lines = file.readlines();
+    
+    for line in lines:
+        pos = line.find(';');
+        
+        if pos != -1:
+            if line[pos-1] == " ":
+                vera.report(filename, lineCounter, ''.join(['White space before semicolon detected at position ', str(pos)]));
+
+        lineCounter = lineCounter + 1;
+
+    file.close();
