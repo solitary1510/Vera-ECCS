6ae848ce72c840dd8d1e4eb253be82cb5c015f52
Phuong H.D Nguyen <solitary1510@gmail.com>
2015-11-02 23:33:49 +0700

    fix rule 3.1.m

--- 1212299_1212504/rules/rule31m.py
+++ 1212299_1212504/rules/rule31m.py
@@ -4,12 +4,12 @@ for filename in vera.getSourceFileNames():
     file = open(filename, 'rb');
     lineCounter = 1;
     lines = file.readlines();
-    
+
     for line in lines:
         pos = line.find(';');
-        
+
         if pos != -1:
-            if line[pos-1] == " ":
+            if line[pos-1] == ' ':
                 vera.report(filename, lineCounter, ''.join(['White space before semicolon detected at position ', str(pos)]));
 
         lineCounter = lineCounter + 1;
