7cb141122b5fef8d4637a2949bd19973db5c8a3a
Phuong H.D Nguyen <solitary1510@gmail.com>
2015-11-02 23:08:14 +0700

    Chang tab to space

--- 1212299_1212504/rules/S001.py
+++ 1212299_1212504/rules/S001.py
@@ -1,14 +1,14 @@
 for filename in vera.getSourceFileNames():
-	file = open(filename, 'rb');
-	lineCounter = 1;
-	lines = file.readlines();
-	
-	for line in lines:
-		pos = line.find('\r');
-		
-		if pos != -1 and pos != len(line) - 1:
-			vera.report(filename, lineCounter, ''.join(['\\r (CR) detected in isolation at position ', str(pos)]));
+    file = open(filename, 'rb');
+    lineCounter = 1;
+    lines = file.readlines();
+    
+    for line in lines:
+        pos = line.find('\r');
+        
+        if pos != -1 and pos != len(line) - 1:
+            vera.report(filename, lineCounter, ''.join(['\\r (CR) detected in isolation at position ', str(pos)]));
 
-		lineCounter = lineCounter + 1;
+        lineCounter = lineCounter + 1;
 
-	file.close();
+    file.close();
