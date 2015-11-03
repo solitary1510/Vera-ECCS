034c9aaf121b7dba4894f3139ed676887fdeb8fd
Uyen Nguyen Thi Kim <ntkuyen0410@gmail.com>
2015-11-03 17:32:00 +0700

    Rule_51b

---
+++ 1212299_1212504/rules/rule51b.py
@@ -0,0 +1,22 @@
+# All new structures, unions, and enumerations shall be named via a typedef
+import re
+typeDef = 'typedef ';
+size_typeDef = 8;
+string_check = re.compile('struct|union|enum');
+
+for filename in vera.getSourceFileNames():
+    file = open (filename, 'rb');
+    lineCounter = 1;
+    lines = file.readlines();
+
+    for line in lines:
+        iterator = re.finditer(string_check, line);
+        if iterator != None:
+            for mem in iterator:
+                if iterator.start() > size_typeDef:
+                    if not re.match(line[(iterator.start() - size_typeDef): iterator.start() - 1], typeDef):
+                        vera.report (filename, lineCounter, ''.join(['position ', str(iterator.start())]));
+                
+        lineCounter = lineCounter + 1;
+        
+    file.close();
