c3a9ea28fc5c141edac424f19cb448215e663d16
Uyen Nguyen Thi Kim <ntkuyen0410@gmail.com>
2015-11-03 20:52:06 +0700

    Fix_rule85a

--- 1212299_1212504/rules/rule85a.py
+++ 1212299_1212504/rules/rule85a.py
@@ -11,7 +11,7 @@ for filename in vera.getSourceFileNames():
         iterator = re.finditer(string_check, line);
         if iterator != None:
             for mem in iterator:
-                vera.report (filename, lineCounter, ''.join(['position ', str(iterator.start())]));
+                vera.report (filename, lineCounter, ''.join(['position ', str(mem.start())]));
         lineCounter = lineCounter + 1;
         
     file.close();
