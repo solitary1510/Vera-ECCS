b17488ba903e9bc36c5eee9271c4dc44fa4f615c
Phuong H.D Nguyen <solitary1510@gmail.com>
2015-11-04 00:03:23 +0700

    Add rule33c
    Still had problem when read file that had blank lines at the end of file

---
+++ 1212299_1212504/rules/rule33c.py
@@ -0,0 +1,15 @@
+# Rule 3.1.c: Each source file shall have a blank line at the end.
+import re
+blank_line = re.compile('^(\s*)$');
+
+for filename in vera.getSourceFileNames():
+    file = open(filename, 'rb');
+    lineCounter = 0;
+    lines = file.readlines();
+    
+    for line in lines:
+        lineCounter = lineCounter + 1;
+    if re.search(blank_line, line) == None:
+        vera.report(filename, lineCounter, 'Source file shall have a blank line at the end');
+
+    file.close();
--- code_need_check.c
+++ code_need_check.c
@@ -1,4 +1,5 @@
 #include "stdio.h"
+
 #define MAX(A, B) (A > B) ? A : B
 #define MAX(A, B) ((A) > (B) ? (A) : (B))
 
