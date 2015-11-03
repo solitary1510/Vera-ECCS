529269be5d62e5d98314c75c997d87a4b8d864c1
Phuong H.D Nguyen <solitary1510@gmail.com>
2015-11-03 14:03:25 +0700

    Clarity parametter vera

--- run_vera.bat
+++ run_vera.bat
@@ -1,5 +1,5 @@
 @echo off
 set PATH=%PATH%;".\vera++";".\vera++\bin";
 set VERA_ROOT=.\1212299_1212504
-vera++ -p 1212299_1212504_rules code_need_check.c
+vera++ --profile 1212299_1212504_rules code_need_check.c
 PAUSE
