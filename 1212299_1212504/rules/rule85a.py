de58d9ff66704dce27fccea6e70efc368f4cfbaf
Uyen Nguyen Thi Kim <ntkuyen0410@gmail.com>
2015-11-03 18:05:52 +0700

    Rule85a

---
+++ 1212299_1212504/rules/rule85a.py
@@ -0,0 +1,17 @@
+# Keywords goto, continue, and break shall not be used to create unconditional jumps
+import re
+string_check = re.compile('goto|continue|break');
+
+for filename in vera.getSourceFileNames():
+    file = open (filename, 'rb');
+    lineCounter = 1;
+    lines = file.readlines();
+
+    for line in lines:
+        iterator = re.finditer(string_check, line);
+        if iterator != None:
+            for mem in iterator:
+                vera.report (filename, lineCounter, ''.join(['position ', str(iterator.start())]));
+        lineCounter = lineCounter + 1;
+        
+    file.close();
