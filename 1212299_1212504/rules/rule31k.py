a3e1382e00e9788d811f832c9c183ef259ad7bb0
Uyen Nguyen Thi Kim <ntkuyen0410@gmail.com>
2015-11-03 15:27:16 +0700

    rule31k

---
+++ 1212299_1212504/rules/rule31k.py
@@ -0,0 +1,21 @@
+# Comma should not be preceded by whitespace, but should be followed by one
+import re
+group_param_func = re.compile('\(.*\)');
+comma_not_space_followed = re.compile(',[^\s]');
+comma_not_space_preceded = re.compile('[\s],');
+
+for filename in vera.getSourceFileNames():
+    file = open (filename, 'rb');
+    lineCounter = 1;
+    lines = file.readlines();
+
+    for line in lines:
+        pos = re.search(group_param_func, line);
+        if pos != None:
+            for mem in re.finditer(comma_not_space_followed, line):
+                vera.report ('Line count: ', lineCounter, 'Index:', mem.start());
+            for mem in re.finditer (comma_not_space_preceded, line):
+                vera.report ('Line count: ', lineCounter, 'Index:', mem.start());
+        lineCounter = lineCounter + 1;
+        
+    file.close();
