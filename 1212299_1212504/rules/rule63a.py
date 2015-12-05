3c43e5665a47c9b06d7281c8247ce3d7bc659605
Phuong H.D Nguyen <solitary1510@gmail.com>
2015-12-05 10:55:39 +0700

    Update reason

---
+++ 1212299_1212504/rules/rule63a.py
@@ -0,0 +1,2 @@
+# Rule 6.3.a: Parameterized macros shall not be used if an inline function can be written to accomplish the same task.
+# Khó xác định được ý nghĩa của một macros để xem nó có thể viết được thành một inline function
