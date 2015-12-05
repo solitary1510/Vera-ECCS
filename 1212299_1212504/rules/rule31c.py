23cda9a436e4526c1efe6b0026342a149f95914e
Phuong H.D Nguyen <solitary1510@gmail.com>
2015-12-05 11:40:38 +0700

    Update error report

--- 1212299_1212504/rules/rule31c.py
+++ 1212299_1212504/rules/rule31c.py
@@ -16,15 +16,15 @@ for filename in vera.getSourceFileNames():
             for mem in iterator:
                 # truoc khong co dau cach => bao loi
                 if line[mem.start() - 1] != ' ':
-                    vera.report (filename, lineCounter, ''.join(['Preceded non have one space at position ', str(mem.start() + 1)]));
+                    vera.report (filename, lineCounter, ''.join(['Operator shall always be preceded and followed by one space at position ', str(mem.start() + 1)]));
                 else: # Sau, kiem tra dau bang => khong co dau cach => bao loi
                     if (line[mem.start() + 1] == '=') and (line[mem.start() + 2] != ' '):
-                        vera.report (filename, lineCounter, ''.join(['Followed non have one space at position ', str(mem.start() + 2)]));
+                        vera.report (filename, lineCounter, ''.join(['Operator shall always be preceded and followed by one space at position ', str(mem.start() + 2)]));
                     else:  # if la mot trong cac ki tu double => kiem tra double=> khong co dau cach => bao loi
                         iterInOperaterDouble = re.search(operatorDouble, line[mem.start()]);
                         if iterInOperaterDouble != None:
                             if (line[mem.start() + 1] == line[mem.start()]) and (line[mem.start() + 2] != ' '):
-                                vera.report (filename, lineCounter, ''.join(['Followed non have one space at position ', str(mem.start() + 2)]));
+                                vera.report (filename, lineCounter, ''.join(['Operator shall always be preceded and followed by one space at position ', str(mem.start() + 2)]));
         lineCounter = lineCounter + 1;
 
     file.close();
