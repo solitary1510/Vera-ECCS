b0cc28280b53328004741eb2ba076508480cfebd
Phuong H.D Nguyen <solitary1510@gmail.com>
2015-12-05 10:59:37 +0700

    Update reason

---
+++ 1212299_1212504/rules/rule71m.py
@@ -0,0 +1,5 @@
+# Rule 7.1.m: The names of all integer variables containing �effectively Boolean� information 
+# (i.e., 0 vs. nonzero) shall begin with the letter �b� and phrased as the question they answer.
+# For example, b_done_yet or gb_is_buffer_full.
+
+# Rule này tùy thuộc vào cách sử dụng của biến để biết được đây là biến chỉ có giá trị true hay false
