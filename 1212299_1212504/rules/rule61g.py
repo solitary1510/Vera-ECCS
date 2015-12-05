46ee8a1c276982bac24df7fd8e86a13b4c54e62b
Phuong H.D Nguyen <solitary1510@gmail.com>
2015-12-05 10:54:07 +0700

    Update reason

---
+++ 1212299_1212504/rules/rule61g.py
@@ -0,0 +1,4 @@
+# Rule 6.1.g: Underscores shall be used to separate words in procedure names.
+# Rule này phụ thuộc nhiều vào người dùng và ngôn ngữ. Có thể trong tên hàm sẽ có viết tắt hoặc
+# các định nghĩa đặc thù trong lĩnh vực nào đó nên rất khó có thể xác định trong một tên hàm các 
+# từ riêng biệt.
