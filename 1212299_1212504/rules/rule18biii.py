c8c9ad707ab028f3192a7d4be095966df253becb
Phuong H.D Nguyen <solitary1510@gmail.com>
2015-12-05 10:51:24 +0700

    Update reason for rule 1.8.b.iii

---
+++ 1212299_1212504/rules/rule18biii.py
@@ -0,0 +1,6 @@
+# 1.8.b.iii: The const keyword shall be used whenever appropriate to define fields in structs
+# and unions that should not be modified
+# (e.g., in a struct overlay for memory-mapped I/O peripheral registers)
+
+# Luật này có nhiều nhập nhằng. Vì khó có thể biết được thuộc tính nào là những thuộc tính không
+# được phép thay đổi như những map về các thanh ghi đọc/ghi......
